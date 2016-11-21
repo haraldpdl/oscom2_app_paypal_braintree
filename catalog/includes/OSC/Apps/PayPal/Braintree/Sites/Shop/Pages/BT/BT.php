7f9640490c8c3cc0a3f4b6695797ddb3c48916df
Harald Ponce de Leon <hpdl@oscommerce.com>
2016-11-21 07:03:21 +0100

    support App Shipping Modules

--- catalog/includes/OSC/Apps/PayPal/Braintree/Sites/Shop/Pages/BT/BT.php
+++ catalog/includes/OSC/Apps/PayPal/Braintree/Sites/Shop/Pages/BT/BT.php
@@ -321,29 +321,46 @@ class BT extends \OSC\OM\PagesAbstract
                             }
                         }
 
-                        if (strpos($_SESSION['shipping'], '_')) {
+                        $OSCOM_SM = null;
+
+                        if (strpos($_SESSION['shipping'], '\\') !== false) {
+                            list($vendor, $app, $module) = explode('\\', $_SESSION['shipping']);
+                            list($module, $method) = explode('_', $module);
+
+                            $module = $vendor . '\\' . $app . '\\' . $module;
+
+                            $code = 'Shipping_' . str_replace('\\', '_', $module);
+
+                            if (Registry::exists($code)) {
+                                $OSCOM_SM = Registry::get($code);
+                            }
+                        } else {
                             list($module, $method) = explode('_', $_SESSION['shipping']);
 
-                            if (is_object($GLOBALS[$module]) || ($_SESSION['shipping'] == 'free_free')) {
-                                if ($_SESSION['shipping'] == 'free_free') {
-                                    $quote[0]['methods'][0]['title'] = OSCOM::getDef('free_shipping_title');
-                                    $quote[0]['methods'][0]['cost'] = '0';
-                                } else {
-                                    $quote = $shipping_modules->quote($method, $module);
-                                }
+                            if (is_object($GLOBALS[$module])) {
+                                $OSCOM_SM = $GLOBALS[$module];
+                            }
+                        }
 
-                                if (isset($quote['error'])) {
-                                    unset($_SESSION['shipping']);
+                        if (isset($OSCOM_SM) || ($_SESSION['shipping'] == 'free_free')) {
+                            if ($_SESSION['shipping'] == 'free_free') {
+                                $quote[0]['methods'][0]['title'] = OSCOM::getDef('free_shipping_title');
+                                $quote[0]['methods'][0]['cost'] = '0';
+                            } else {
+                                $quote = $shipping_modules->quote($method, $module);
+                            }
 
-                                    OSCOM::redirect('checkout_shipping.php');
-                                } else {
-                                    if ((isset($quote[0]['methods'][0]['title'])) && (isset($quote[0]['methods'][0]['cost']))) {
-                                        $_SESSION['shipping'] = [
-                                            'id' => $_SESSION['shipping'],
-                                            'title' => (($free_shipping == true) ?  $quote[0]['methods'][0]['title'] : $quote[0]['module'] . ' ' . $quote[0]['methods'][0]['title']),
-                                            'cost' => $quote[0]['methods'][0]['cost']
-                                        ];
-                                    }
+                            if (isset($quote['error'])) {
+                                unset($_SESSION['shipping']);
+
+                                OSCOM::redirect('checkout_shipping.php');
+                            } else {
+                                if ((isset($quote[0]['methods'][0]['title'])) && (isset($quote[0]['methods'][0]['cost']))) {
+                                    $_SESSION['shipping'] = [
+                                        'id' => $_SESSION['shipping'],
+                                        'title' => (($free_shipping == true) ? $quote[0]['methods'][0]['title'] : $quote[0]['module'] . (isset($quote[0]['methods'][0]['title']) && !empty($quote[0]['methods'][0]['title']) ? ' (' . $quote[0]['methods'][0]['title'] . ')' : '')),
+                                        'cost' => $quote[0]['methods'][0]['cost']
+                                    ];
                                 }
                             }
                         }
